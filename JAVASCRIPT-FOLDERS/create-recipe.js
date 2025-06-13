b32fb33ad4c5a8921ea58ebf0add81c47f8483bb
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-06-13 23:45:02 +0530

    Implement a Create new recipe post function

--- JAVASCRIPT-FOLDERS/create-recipe.js
+++ JAVASCRIPT-FOLDERS/create-recipe.js
@@ -1,5 +1,4 @@
-import { db,  auth } from "../firebase-config.js"
-
+import { db, auth } from "../firebase-config.js"
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js"
 import {
   doc,
@@ -17,7 +16,7 @@ let description = document.getElementById('description');
 let ingredients = document.getElementsByName('ingredients');
 let preparationSteps = document.getElementById('preparation-steps');
 let hours = document.getElementById('hour');
-let minutes = document.getElementById('minutes');
+let minute = document.getElementById('minute');
 let difficulty = document.getElementById('difficulty');
 let recipeImage = document.getElementById('recipe-image')
 let addIngredientButton = document.getElementById('add-ingredient-btn');
@@ -27,42 +26,62 @@ let ingredientSection = document.getElementById('ingredient-section')
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
   // This function is  for add new input pair before add button 
   if (addIngredientButton) {
+
     addIngredientButton.addEventListener('click', () => {
+
       let newInput = document.createElement('input')
       newInput.placeholder = "Ingredient with Quantity"
       newInput.name = "ingredients"
       newInput.type = "text"
-      let buttons = document.createElement('button')
-      buttons.innerText = "delete button"
-      ingredientSection.appendChild(buttons)
       ingredientSection.insertBefore(newInput, addIngredientButton)
     });
-
-
-    onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        let currentUser = user;
-        console.log(currentUser)
-
-
-        const userDetails = await getDoc(doc(db, "users", currentUser.uid));
-
-        if(userDetails.exists()){
-          const data = userDetails.data()
-          console.log(data.name)
-        }
-      }
-
-
-    })
   }  //if addIngredientButton
 
 
+  // post button { add recipes in databse}
+  if (postButton) {
+    postButton.addEventListener('click', () => {
+      // collect ingredients as array. {array.form Converts that NodeList into a real array  \\ map -> goes through each input and extract value  || trim remove white space  || filter check is any input field is not empty}
+      let ingredientsList = Array.from(ingredients).map(input => input.value.trim()).filter(val => val !== "");
+
+      //  collect other from data in object form
+
+      const recipeData = {
+        Title: recipeTitle.value.trim(),
+        Category: category.value.trim(),
+        Description: description.value.trim(),
+        Ingredients: ingredientsList,
+        PreparationSteps: preparationSteps.value.trim(),
+        CookingTime: {
+          Hour: hours.value.trim() || 0,
+          Minute: minute.value.trim() || 0
+        },
+        Difficulty: difficulty.value,
+        ImageUrl: recipeImage.files[0] ? recipeImage.files[0].name : "",
+        CreatedAt: new Date()
+      }
 
 
-
+      console.log(recipeData)
+      // Check if user is logged in
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          const userID = user.uid;
+          try {
+            const recipeRef = collection(db, "users", userID, "recipes");
+            await addDoc(recipeRef, recipeData);
+            alert("Recipe posted successfully!");
+          } catch (error) {
+            console.log(`Error adding recipes :  ${error}`)
+            alert("Error adding recipe");
+          }
+        } else {
+          alert('Please login to post a recipe.');
+        }
+      })
+    }); //    postButton.addEventListener
+  }  // if postButton 
 
 });   // DOMContentLoaded
