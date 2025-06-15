9fa3f5df9b8d436e50565ef11a9318f198084a14
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-06-15 11:22:52 +0530

    correction

--- JAVASCRIPT-FOLDERS/recipes.js
+++ JAVASCRIPT-FOLDERS/recipes.js
@@ -13,10 +13,11 @@ async function getAllRecipes() {
     allRecipes.push({ id: recipe.id, ...recipe.data() })
   });
 
-  filteredRecipe(allRecipes);
-}
+  displayRecipes(allRecipes)
 
-getAllRecipes()
+}
+ getAllRecipes() 
+
 // display function
 function displayRecipes(allRecipes) {
   allRecipes.forEach((recipe) => {
@@ -25,20 +26,19 @@ function displayRecipes(allRecipes) {
 
     console.log(recipe)
     cardDiv.innerHTML = `
-
-          <div class="user-details">
+            <div class="user-details">
             <div class="user-profile-photo">
               <img src="../ASSETS/profile.png" alt="user profile photo">
             </div>
             <div class="user-name-date">
               <h2>Abhishek verma</h2>
-              <p>${recipe.CreatedAt}</p>
-              </div>
-              <button>Follow</button>
+              <p>22-03-2025</p>
+            </div>
+            <button>Follow</button>
           </div>
           <div class="recipe-media">
             <img src="../ASSETS/dinner.jpg" alt="recipe-media">
-            </div>
+          </div>
           <div class="controls">
             <div class="like">
               <i class="fa-regular fa-heart" style="color: #ff1a1a;"></i>
@@ -47,32 +47,21 @@ function displayRecipes(allRecipes) {
             <div class="share">
               <i class="fa-solid fa-share" style="color: #ffc800;"></i>
               <p>Share</p>
-              </div>
-              <h2>Recipe Title</h2>
-            <h2><i class="fa-regular fa-bookmark" style="color: #fa7900;"></i></h2>
             </div>
-            <div class="descriptionn">
+            <h2>Recipe Title</h2>
+            <h2><i class="fa-regular fa-bookmark" style="color: #fa7900;"></i></h2>
+          </div>
+          <div class="descriptionn">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ea in iusto natus ab eos non iure
-            quisquam
+              quisquam
               dolorum. Illo ut harum dignissimos laudantium quidem possimus, facere placeat voluptatem ex quae quaerat
               atque unde obcaecati distinctio accusamus. Provident, sapiente at.</p>
-              </div>
-  `;
-    recipeCardsContainer.appendChild(cardDiv)
-  })
-}
-
-let mealTypes = document.getElementById('meal-types');
-mealTypes.addEventListener('change',()=>{
+          </div>
   
-function filteredRecipe(allRecipes) {
-  let recipes = allRecipes
-  let filtered = recipes.filter((recipe) => {
-    return recipe.Title === mealTypes.value
+  `;
+
+  recipeCardsContainer.appendChild(cardDiv)
+
   })
 
-  displayRecipes(filtered)
 }
-
-})
-
